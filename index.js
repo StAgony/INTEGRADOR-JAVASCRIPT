32898bbdbe81757dd5bcce8783426e918f14ea93
StAgony <santiago_almada@outlook.com>
2022-11-25 16:23:20 -0300

    v1

---
+++ index.js
@@ -0,0 +1,114 @@
+const contenedor = document.querySelector(".items");
+
+const contenedorCategorias = document.querySelector(".categorias");
+
+const listaCategorias = document.querySelectorAll(".boton") ;
+
+const botonVermas = document.querySelector(".boton-vermas");
+
+const carro = document.querySelector(".carrito");
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+const saveLocalStorage = (listaCompras) => {
+  localStorage.setItem("carrito", JSON.stringify(listaCompras));
+};
+////////////////////render
+
+const renderProducto = (producto) => {
+    const {id, nombre, precio} = producto
+    return `
+    <div class="productoU">
+        <img class="imagen-producto" src="./productos fotos/${id}.png" alt="">
+        <div class="mid">
+            <h4>${nombre}</h4>
+            <h4>${precio}</h4>
+        </div>
+        <div class="bot">
+            <button>Comprar</button>
+        </div>
+    </div>
+    `
+}
+///////////// render de los todos los productos divividos 
+const renderTodo = (index = 0 ) => {
+    contenedor.innerHTML += controladorproductos.ProductosDivididos[index].map(renderProducto).join('');
+}
+//////////////// render por filtro 
+const renderFiltrados = (categoria) => {
+    const Lista = productosdata.filter(
+    (item) => item.categoria === categoria
+    );
+    contenedor.innerHTML = Lista.map(renderProducto).join('');
+};
+
+//////////////// renderizador general 
+const renderProductos = (index = 0, categoria = undefined) => {
+    if (!categoria) {
+        renderTodo(index);
+        return;
+        }
+    renderFiltrados(categoria);
+};
+
+/////////////////// cambiador de filtro 
+const Cambiarfiltro = (nuevaCategoria) => {
+    const categorias = [...listaCategorias];
+    categorias.forEach((datasetCategoria) => {
+    if (datasetCategoria.dataset.categoria !== nuevaCategoria) {
+    datasetCategoria.classList.remove("activo");
+    return;
+    }
+    else {
+    datasetCategoria.classList.add("activo");
+    }
+    });
+};
+
+//////////////////////// quitar ver mas si hay categoria
+const quitarVerMas = (c) => {
+    if (!c) {
+        botonVermas.classList.remove("oculto");
+        return;
+        }
+        botonVermas.classList.add("oculto");
+};
+
+///////////////  
+const accionfiltro = (e) => {
+    const categoriaseleccionada = e.target.dataset.categoria;
+    Cambiarfiltro(categoriaseleccionada);
+    quitarVerMas(categoriaseleccionada);
+};
+///////////////////////// aplicar filtros
+const aplicarFiltro = (e) => {
+    if (!e.target.classList.contains("boton")) return;
+    accionfiltro(e);
+    if (!e.target.dataset.categoria) {
+      contenedor.innerHTML = "";
+      renderProductos();
+    } else {
+      renderProductos(0, e.target.dataset.categoria);
+      controladorproductos.SiguientesProductos = 1;
+    }
+};
+
+const isLastIndexOF = () =>
+controladorproductos.SiguientesProductos === controladorproductos.LimiteProductos;
+
+const mostrarmas = () => {
+  renderProductos(controladorproductos.SiguientesProductos);
+  controladorproductos.SiguientesProductos = controladorproductos.SiguientesProductos + 1
+  if (isLastIndexOF()) {
+    botonVermas.classList.add("oculto");
+  }
+};
+
+
+const init = () => {
+    renderProductos();
+    contenedorCategorias.addEventListener('click', aplicarFiltro)
+    botonVermas.addEventListener('click', mostrarmas)
+}
+
+init();
---
+++ products.js
@@ -0,0 +1,349 @@
+let p1 = {
+    id : 1,
+    precio: 2300,
+    nombre : 'noname',
+    categoria: 'mochilas', 
+}
+let p2 = {
+    id : 2,
+    precio: 21300,
+    nombre : 'noname',
+    categoria: 'mochilas', 
+}
+let p3 = {
+    id : 3,
+    precio: 2500,
+    nombre : 'noname',
+    categoria: 'mochilas', 
+}
+let p4 = {
+    id : 4,
+    precio: 7300,
+    nombre : 'noname',
+    categoria: 'mochilas', 
+}
+let p5 = {
+    id : 5,
+    precio: 6300,
+    nombre : 'noname',
+    categoria: 'mochilas', 
+}
+let p6 = {
+    id : 6,
+    precio: 300,
+    nombre : 'noname',
+    categoria: 'mochilas', 
+}
+let p7 = {
+    id : 7,
+    precio: 2000,
+    nombre : 'noname',
+    categoria: 'mochilas', 
+}
+let p8 = {
+    id : 8,
+    precio: 2000,
+    nombre : 'noname',
+    categoria: 'mochilas', 
+}
+let p9 = {
+    id : 9,
+    precio: 2300,
+    nombre : 'noname', 
+    categoria: 'calzado',
+}
+let p10 = {
+    id : 10,
+    precio: 230,
+    nombre : 'noname', 
+    categoria: 'calzado',
+}
+let p11 = {
+    id : 11,
+    precio: 8000,
+    nombre : 'noname', 
+    categoria: 'calzado',
+}
+let p12 = {
+    id : 12,
+    precio: 9500,
+    nombre : 'noname',
+    categoria: 'calzado', 
+}
+let p13 = {
+    id : 13,
+    precio: 500,
+    nombre : 'noname',
+    categoria: 'calzado', 
+}
+let p14 = {
+    id : 14,
+    precio: 8400,
+    nombre : 'noname',
+    categoria: 'calzado', 
+}
+let p15 = {
+    id : 15,
+    precio: 6700,
+    nombre : 'noname',
+    categoria: 'calzado', 
+}
+let p16 = {
+    id : 16,
+    precio: 2800,
+    nombre : 'noname',
+    categoria: 'calzado', 
+}
+let p17 = {
+    id : 17,
+    precio: 6500,
+    nombre : 'noname',
+    categoria: 'calzado', 
+}
+let p18 = {
+    id : 18,
+    precio: 1400,
+    nombre : 'noname',
+    categoria: 'calzado', 
+}
+let p19 = {
+    id : 19,
+    precio: 100,
+    nombre : 'noname',
+    categoria: 'calzado', 
+}
+let p20 = {
+    id : 20,
+    precio: 2,
+    nombre : 'noname',
+    categoria: 'vestidos', 
+}
+let p21 = {
+    id : 21,
+    precio: 83100,
+    nombre : 'noname',
+    categoria: 'vestidos', 
+}
+let p22 = {
+    id : 22,
+    precio: 8500,
+    nombre : 'noname',
+    categoria: 'vestidos', 
+}
+let p23 = {
+    id : 23,
+    precio: 1200,
+    nombre : 'noname',
+    categoria: 'vestidos', 
+}
+let p24 = {
+    id : 24,
+    precio: 1300,
+    nombre : 'noname',
+    categoria: 'vestidos', 
+}
+let p25 = {
+    id : 25,
+    precio: 1500,
+    nombre : 'noname',
+    categoria: 'vestidos', 
+}
+let p26 = {
+    id : 26,
+    precio: 9500,
+    nombre : 'noname',
+    categoria: 'vestidos', 
+}
+let p27 = {
+    id : 27,
+    precio: 8200,
+    nombre : 'noname',
+    categoria: 'vestidos', 
+}
+let p28 = {
+    id : 28,
+    precio: 700,
+    nombre : 'noname',
+    categoria: 'gorros', 
+}
+let p29 = {
+    id : 29,
+    precio: 900,
+    nombre : 'noname',
+    categoria: 'gorros', 
+}
+let p30 = {
+    id : 30,
+    precio: 1000,
+    nombre : 'noname', 
+    categoria: 'gorros',
+}
+let p31 = {
+    id : 31,
+    precio: 3500,
+    nombre : 'noname', 
+    categoria: 'gorros',
+}
+let p32 = {
+    id : 32,
+    precio: 1500,
+    nombre : 'noname',
+    categoria: 'gorros', 
+}
+let p33 = {
+    id : 33,
+    precio: 3200,
+    nombre : 'noname', 
+    categoria: 'gorros',
+}
+let p34 = {
+    id : 34,
+    precio: 1200,
+    nombre : 'noname',
+    categoria: 'gorros', 
+}
+let p35 = {
+    id : 35,
+    precio: 2300,
+    nombre : 'noname', 
+    categoria: 'gorros',
+}
+let p36 = {
+    id : 36,
+    precio: 600,
+    nombre : 'noname', 
+    categoria: 'gorros',
+}
+let p37 = {
+    id : 37,
+    precio: 5600,
+    nombre : 'noname', 
+    categoria: 'gorros',
+}
+let p38 = {
+    id : 38,
+    precio: 4500,
+    nombre : 'noname',
+    categoria: 'pantalones', 
+}
+let p39 = {
+    id : 39,
+    precio: 5400,
+    nombre : 'noname',
+    categoria: 'pantalones', 
+}
+let p40 = {
+    id : 40,
+    precio: 2300,
+    nombre : 'noname',
+    categoria: 'pantalones', 
+}
+let p41 = {
+    id : 41,
+    precio: 200,
+    nombre : 'noname',
+    categoria: 'pantalones', 
+}
+let p42 = {
+    id : 42,
+    precio: 6800,
+    nombre : 'noname', 
+    categoria: 'pantalones',
+}
+let p43 = {
+    id : 43,
+    precio: 3500,
+    nombre : 'noname',
+    categoria: 'pantalones', 
+}
+let p44 = {
+    id : 44,
+    precio: 2300,
+    nombre : 'noname',
+    categoria: 'pantalones', 
+}
+let p45 = {
+    id : 45,
+    precio: 2400,
+    nombre : 'noname',
+    categoria: 'pantalones', 
+}
+let p46 = {
+    id : 46,
+    precio: 6800,
+    nombre : 'noname',
+    categoria: 'pantalones', 
+}
+let p47 = {
+    id : 47,
+    precio: 9700,
+    nombre : 'noname',
+    categoria: 'pantalones', 
+}
+let p48 = {
+    id : 48,
+    precio: 6500,
+    nombre : 'noname', 
+    categoria: 'remeras',
+}
+let p49 = {
+    id : 49,
+    precio: 1300,
+    nombre : 'noname', 
+    categoria: 'remeras',
+}
+let p50 = {
+    id : 50,
+    precio: 3100,
+    nombre : 'noname', 
+    categoria: 'remeras',
+}
+let p51 = {
+    id : 51,
+    precio: 23000,
+    nombre : 'noname', 
+    categoria: 'remeras',
+}
+let p52 = {
+    id : 52,
+    precio: 8500,
+    nombre : 'noname', 
+    categoria: 'remeras',
+}
+let p53= {
+    id : 53,
+    precio: 7800,
+    nombre : 'noname',
+    categoria: 'remeras', 
+}
+let p54 = {
+    id : 54,
+    precio: 90,
+    nombre : 'noname',
+    categoria: 'remeras', 
+}
+
+const mochilas = [p1,p2,p3,p4,p5,p6,p7,p8]
+const calzado = [p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19]
+const vestidos = [p20,p21,p22,p23,p24,p25,p26,p27]
+const gorros = [p28,p29,p30,p31,p32,p33,p34,p35,p36,p37]
+const pantalones = [p38,p39,p40,p41,p42,p43,p44,p45,p46,p47]
+const remeras = [p48,p49,p50,p51,p52,p53,p54]
+
+const productos = [mochilas,calzado,vestidos,gorros,pantalones,remeras]
+
+const productosdata = [p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22,p23,p24,p25,p26,p27,p28,p29,p30,p31,p32,p33,p34,p35,p36,p37,p38,p39,p40,p41,p42,p43,p44,p45,p46,p47,p48,p49,p50,p51,p52,p53,p54]
+
+/////////////////////////////////////////////////////////////////////////////////////////
+const SeparadorProductos = (tamaño) => {
+    ProductosDivididos = [];
+    for (let i=0; i < productosdata.length; i += tamaño)
+        ProductosDivididos.push (productosdata.slice(i, i + tamaño));
+    return ProductosDivididos;
+}
+
+const controladorproductos = {
+    ProductosDivididos: SeparadorProductos(4),
+    SiguientesProductos: 1,
+    LimiteProductos: SeparadorProductos(4).length,
+}
